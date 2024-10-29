80e6b166893bd4597adea5f4f75554638e4ea7ec
fahledu <fahledu@gmail.com>
2024-10-29 19:46:58 -0300

    config express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express, { Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World')
+});
+
+app.listen(port, () => {
+    console.log(`Server running at: http://localhost:${port}`)
+})
