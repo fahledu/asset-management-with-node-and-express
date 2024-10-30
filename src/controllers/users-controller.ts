1e89f875f562985d6a9956941631bb9a40082c83
fahledu <fahledu@gmail.com>
2024-10-30 20:03:24 -0300

    layer utils created

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import { getUsersService } from "../services/users-services";
+import { ok } from "../utils/http-helper";
 
 export const getUser = async (req:Request, res: Response) => {
 
     const data = await getUsersService();
 
-    res.status(200).json(data);
+    const response = await ok(data);
+
+    res.status(response.statusCode).json(response.body);
 };
